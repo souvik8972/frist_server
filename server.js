32b08b0ce0525b29f4ea873bdf5fb030133faa63
souvik das <souvik8582@gmail.com>
2023-09-03 12:16:36 +0530

    server home,about

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   console.log("Souvik")
+// });
+
+
+
+// server.listen(4000)
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log("Souvik")
+  // Get the URL from the request object
+  const url = req.url;
+
+  // Set the response content type to plain text
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Check the URL and provide custom responses
+  if (url === '/home') {
+    res.end('Welcome home\n');
+  } else if (url === '/about') {
+    res.end('Welcome to About Us page\n');
+  } else if (url === '/node') {
+    res.end('Welcome to my Node.js project\n');
+  } else {
+    // For any other URL, return a generic message
+    res.end('Welcome to my website\n');
+  }
 });
 
+const port = 4000;
+console.log(`Server is running on http://localhost:${port}`)
+
+server.listen(port)
 
 
-server.listen(4000)
